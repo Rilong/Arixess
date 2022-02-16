8a075b0a46f18cb0030db6cafbb45b1fab790099
rilong <romgnatyuk@gmail.com>
2022-02-16 13:32:33 +0200

    Select all the messages if authorized user with a role of a manager

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $messages = [];
+
+        if (Auth::user()->isManager()) {
+            $messages = Message::with('user')->orderByDesc('id')->get();
+        }
+
+        return view('home', compact('messages'));
     }
 }
