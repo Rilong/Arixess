ffe09af6409cbe15567b9ed9b8514329e9b75858
rilong <romgnatyuk@gmail.com>
2022-02-16 13:36:57 +0200

    Added action for toggle read and refactoring

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,33 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\MessageRequest;
+use App\Jobs\SendEmail;
 use App\Models\Message;
-use App\Models\User;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
+    /**
+     * Processes a contact form
+     *
+     * @param MessageRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function create(MessageRequest $request) {
-        $uploadedFile = $request->file('file');
 
         $data = ['user_id' => Auth::id()];
 
-        $lastMessage = Message::getLastBetweenDay(Auth::id());
-        if ($lastMessage) {
+        if (Message::getLastWithinDay(Auth::id())) {
             return redirect()->route('home')->with('timeError', 'Можно отправлять сообщения раз в сутки');
         }
 
         if ($request->hasFile('file')) {
-            $data['filename'] = uploadFile($uploadedFile);
+            $data['filename'] = uploadFile($request->file('file'));
         }
-
 
         $data = array_merge($data, $request->all('subject', 'text'));
         $message = Message::create($data);
 
-        // Dispatch a mail job
+        SendEmail::dispatch($message->getDetails());
 
         return redirect()->route('home')->with('success', 'Ваше сообщение было отправлено успешно. Спасибо');
     }
+
+    /**
+     * Toggles read field by message id
+     *
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+
+    public function toggleRead($id)
+    {
+        $message = Message::find($id);
+        $message->toggleRead();
+        return redirect()->back();
+    }
+
 }
